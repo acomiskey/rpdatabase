5335720a1e3f47b4bce80bcb9b096503aa5a3262
acomiskey <acomiskey@ufl.edu>
2022-05-18 15:28:25 -0400

    Added basics of user interface.

---
+++ src/components/TabSelect.vue
@@ -0,0 +1,76 @@
+<script setup>
+</script>
+
+<template>
+    <div id = "tabs-bar">
+        <div class = "tab">
+            characters
+        </div>
+        <div class = "tab">
+            reserves
+        </div>
+    </div>
+    <div id = "control-bar">
+        <select class = "selector">
+            <option value="all">Writer</option>
+            <option value="test-name">Example Writer Name</option>
+        </select>
+        <select class = "selector">
+            <option value = "all">Fandom</option>
+            <option value = "test-fandom">Example Fandom</option>
+        </select>
+        <select class = "selector">
+            <option value = "default">Sort</option>
+            <option value = "oldest-first">Oldest to Newest</option>
+            <option value = "newest-first">Newest to Oldest</option>
+            <option value = "alpha-char">Alpha by Character</option>
+            <option value = "alpha-fandom">Alpha by Fandom</option>
+            <option value = "alpha-writer">Alpha by Writer</option> 
+        </select>
+    </div>
+    <div id = "results-box">
+        this is where the search results will go
+    </div>
+</template>
+
+<style scoped>
+#tabs-bar
+{
+    width: 100%;
+    height: 50px;
+    background-color: yellow;
+    text-align: left;
+}
+
+#control-bar
+{
+    width: 100%;
+    height: 30px;
+    background-color: purple;
+    text-align: right;
+    padding-right: 20px;
+}
+
+#results-box
+{
+    width: 100%;
+    background-color: navy;
+    padding: 50px;
+}
+
+.tab
+{
+    display: inline-block;
+    height: 100%;
+    width: 200px;
+    background-color: orange;
+    text-align: center;
+    font-size: 14pt;
+    padding-top: 10px;
+}
+
+.selector
+{
+    margin-left: 20px;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,64 @@
+<script setup>
+</script>
+
+<template>
+    <div id = "top-bar">
+        <div id = "site-icon">
+            icon here
+        </div>
+        <div id = "top-buttons">
+            <button class = "nav-button">
+                apply
+            </button>
+            <button class = "nav-button">
+                reserve
+            </button>
+            <button class = "nav-button">
+                about
+            </button>
+            <button class = "nav-button">
+                log in
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#top-bar
+{
+    width: 100%;
+    height: 50px;
+    background-color: red;
+}
+
+#site-icon
+{
+    float: left;
+    height: 44px;
+    width: 44px;
+    margin-top: 3px;
+    margin-left: 3px;
+    background-color: blue;
+    word-wrap: normal;
+    text-align: center;
+}
+
+#top-buttons
+{
+    float: right;
+    height: 100%;
+    width: 90%;
+    background-color: green;
+    padding-right: 10px;
+    padding-top:10px;
+    text-align: right;
+}
+
+.nav-button
+{
+    width: 150px;
+    height: 30px;
+    font-size: 14pt;
+    margin-left: 20px;
+}
+</style>
